a5afacff8d5c5440bd0e554349831fdb06515af1
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:51:55 -0700

    msm8996: Add HIDL manifest

--- msm8996.mk
+++ msm8996.mk
@@ -207,6 +207,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # IMS
 PRODUCT_PACKAGES += \
     libshims_ims
